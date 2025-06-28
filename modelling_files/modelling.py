3b275c13107411d921536da1e6231077de28e2e9
widigdacahya <rizkiwidigda@gmail.com>
2025-06-28 14:11:08 +0700

    fix: retry modelling mlrun stuff

--- modelling_files/modelling.py
+++ modelling_files/modelling.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -7,9 +7,7 @@ from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 import mlflow
 import mlflow.sklearn
 
-def train_with_tuning():
-
-    # Environment CI stuff
+def run_training():
     if os.getenv("CI"):
         print("Running in CI environment. Logging to local 'mlruns' directory.")
         mlflow.set_experiment("CI - Medical Cost Prediction")
@@ -18,29 +16,27 @@ def train_with_tuning():
         mlflow.set_tracking_uri("http://127.0.0.1:5000")
         mlflow.set_experiment("Medical Cost Prediction")
 
-    # CSV path stuff
     script_path = os.path.abspath(__file__)
     script_dir = os.path.dirname(script_path)
     project_root = os.path.dirname(script_dir)
     csv_path = os.path.join(project_root, 'preprocessing', 'MedicalCost_preprocessing', 'insurance_processed.csv')
-
-    df = pd.read_csv(csv_path)
-    mlflow_dataset = mlflow.data.from_pandas(df, source='insurance_processed.csv')
-
     
+    df = pd.read_csv(csv_path)
+    mlflow_dataset = mlflow.data.from_pandas(df, source=csv_path)
+
     X = df.drop('charges', axis=1)
     y = df['charges']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    n_estimators_list = [50, 100, 150]
-    max_depth_list = [5, 10, None] 
+    n_estimators_list = [50, 100]
+    max_depth_list = [5, 10] 
 
+    print("Memulai Hyperparameter Tuning...")
     for n in n_estimators_list:
         for depth in max_depth_list:
             with mlflow.start_run(run_name=f"RF_n_estimators_{n}_max_depth_{depth}", nested=True):
-
-                mlflow.log_input(mlflow_dataset, context="training")
                 
+                mlflow.log_input(mlflow_dataset, context="training")
                 mlflow.log_param("n_estimators", n)
                 mlflow.log_param("max_depth", depth if depth is not None else "None")
 
@@ -63,6 +59,9 @@ def train_with_tuning():
                     artifact_path="random_forest_model", 
                     input_example=X_train.head()
                 )
+                print(f"Run {n}-{depth} selesai.")
+    
+    print("\nSemua eksperimen tuning selesai!")
 
 if __name__ == '__main__':
-    train_with_tuning()
+    run_training()
